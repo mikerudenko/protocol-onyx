fcbcf59d2f477feb69d82ec60f1d83313165c014
Sean Casey <sean@seanjcasey.com>
2025-07-10 17:48:01 -0400

    feat: account erc20 IPositionTracker

--- src/interfaces/IValuationHandler.sol
+++ src/interfaces/IValuationHandler.sol
@@ -14,6 +14,10 @@ pragma solidity ^0.8.0;
 /// @title IValuationHandler Interface
 /// @author Enzyme Foundation <security@enzyme.finance>
 interface IValuationHandler {
+    function convertAssetAmountToValue(address _asset, uint256 _assetAmount) external view returns (uint256 value_);
+
+    function convertValueToAssetAmount(uint256 _value, address _asset) external view returns (uint256 assetAmount_);
+
     function getDefaultSharePrice() external view returns (uint256);
 
     function getSharePrice() external view returns (uint256 price_, uint256 timestamp_);
